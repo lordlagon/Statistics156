87757f34078526cd0c8f982619474a95f887d725
Andre Macedo <andredekamacedo@gmail.com>
2021-06-17 22:15:51 -0300

    include FrontEnd

---
+++ Statistics156-Front/wwwroot/js/Chart.js
@@ -0,0 +1,13 @@
+﻿function GeneratePieChart(countrInfos) {
+    am4core.ready(function () {
+        am4core.useTheme(am4themes_animated);
+        var chart = am4core.create("chartdiv", am4charts.PieChart3D);
+        chart.hiddenState.properties.opacity = 0; 
+        chart.legend = new am4charts.Legend();
+        chart.data = countrInfos;
+        var series = chart.series.push(new am4charts.PieSeries3D());
+        series.dataFields.value = "deathPercent";
+        series.dataFields.category = "name";
+
+    }); 
+}
