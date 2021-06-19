9733cb32a2bc75c42b62c952e7a6a223cd75ed01
Andre Macedo <andredekamacedo@gmail.com>
2021-06-19 12:08:29 -0300

    incluido busca na api do Python

---
+++ Statistics156-Front/Services/Central.cs
@@ -0,0 +1,23 @@
+﻿using Flurl;
+using Statistics156_Front.Data;
+using Flurl.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Statistics156_Front.Services
+{
+    public class Central
+    {
+
+        public async Task<List<CountryInfo>> GetCentralAsync()
+        {
+            var person = await "http://127.0.0.1:5000/"
+                .AppendPathSegment("api")
+                .AppendPathSegment("tasks").GetJsonAsync<List<CountryInfo>>();
+                
+            return person;
+        }
+    }
+}
